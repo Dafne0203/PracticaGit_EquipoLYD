93cd1889dd758808ce289679858df1f7687ffd2b
Laura Penedo <xri39031@educastur.es>
2025-10-31 12:23:02 +0100

    linea 7 laura

--- src/prueba/Alumnos.java
+++ src/prueba/Alumnos.java
@@ -4,7 +4,6 @@ public class Alumnos {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, comos alumnos de 1 DAW");
-
+		System.out.println("Cambio realizado por Laura");
 	}
-
 }
