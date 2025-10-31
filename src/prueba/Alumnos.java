05fc23e3228b28017051b9bb5b74c8cd324e8931
Yhadira Vera <RIB92792@educastur.es>
2025-10-31 12:31:25 +0100

    segundo cambio yhadira

--- src/prueba/Alumnos.java
+++ src/prueba/Alumnos.java
@@ -3,8 +3,8 @@ package prueba;
 public class Alumnos {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, comos alumnos de 1 DAW");
-		System.out.println("Cambio Yhadira");
+		System.out.println("Hola, comos alumnos de 1 DAW, hice cambio");
+		
 
 	}
 
