4a131e78d16552ac6b29d4392106c5407ad263bd
Dario Sanchez <KIY77204@educastur.es>
2025-11-05 13:18:04 +0100

    Modificacion Dario

--- src/prueba/Alumnos.java
+++ src/prueba/Alumnos.java
@@ -3,7 +3,7 @@ package prueba;
 public class Alumnos {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, comos alumnos de 1 DAW");
+		System.out.println("Hola, comos alumnos de 1 DAW, yo soy Dario");
 		System.out.println("Hola, soy Darío Sánchez");
 
 		System.out.println("");
